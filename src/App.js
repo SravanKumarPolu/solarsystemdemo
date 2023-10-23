48b350e2d43885beae2d6a094ae24e59b448afe2
PVSKR <skr.polu@hotmail.com>
2023-10-23 19:22:14 +0530

    planet name created

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="sun">
+          Sun
+          <div class="mercury">Mercury</div>
+          <div class="venus">Venus</div>
+          <div class="earth">
+            Earth
+            <div class="moon">Moon</div>
+          </div>
+          <div class="mars">Mars</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
